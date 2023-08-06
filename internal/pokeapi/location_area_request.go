5f5f76c423ebecd71c4aa33520b8d424df8f70a2
AmelAbema <dogormous14@gmail.com>
2023-08-06 15:12:11 +0200

    tests for caching added

--- internal/pokeapi/location_area_request.go
+++ internal/pokeapi/location_area_request.go
@@ -13,6 +13,20 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 	if pageURL != nil {
 		fullURL = *pageURL
 	}
+
+	dat, ok := c.cache.Get(fullURL)
+	fmt.Println(dat)
+	if ok {
+		fmt.Println("cache hit!")
+		locationAreasResp := LocationAreasResp{}
+		err := json.Unmarshal(dat, &locationAreasResp)
+		if err != nil {
+			return LocationAreasResp{}, err
+		}
+		return locationAreasResp, nil
+	}
+	fmt.Println("cache miss!")
+
 	request, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
 		return LocationAreasResp{}, err
@@ -22,25 +36,29 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 	if err != nil {
 		return LocationAreasResp{}, err
 	}
+
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
 			panic(err)
 		}
 	}(do.Body)
+
 	if do.StatusCode > 399 {
 		return LocationAreasResp{}, fmt.Errorf("bad status code: %v", do.StatusCode)
 	}
-	all, err := io.ReadAll(do.Body)
+	dat, err = io.ReadAll(do.Body)
 	if err != nil {
 		return LocationAreasResp{}, err
 	}
 
 	locationAreasResp := LocationAreasResp{}
-	err = json.Unmarshal(all, &locationAreasResp)
+	err = json.Unmarshal(dat, &locationAreasResp)
 	if err != nil {
 		return LocationAreasResp{}, err
 	}
+
+	c.cache.Add(fullURL, dat)
 
 	return locationAreasResp, nil
 }
--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -1,6 +1,7 @@
 package pokeapi
 
 import (
+	"github.com/AmelAbema/Pokedex/internal/pokecache"
 	"net/http"
 	"time"
 )
@@ -8,11 +9,14 @@ import (
 const baseUrl = "https://pokeapi.co/api/v2"
 
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient() Client {
+func NewClient(interval time.Duration) Client {
+
 	return Client{
+		cache: pokecache.NewCache(interval),
 		httpClient: http.Client{
 			Timeout: time.Minute,
 		},
